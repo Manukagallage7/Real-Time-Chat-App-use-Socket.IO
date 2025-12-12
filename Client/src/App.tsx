62c45cb493767e57a98750720545fa36e6a694dd
Manukagallage7 <manukakavinda1110@gmail.com>
2025-12-12 16:20:09 +0530

    feat: refactor App component to manage socket connection and user state

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,29 +1,53 @@
-import { useEffect } from "react";
+import { useState, useEffect, ChangeEvent, JSX } from "react";
 import { io, Socket } from "socket.io-client";
 
 const App = ()=> {
-  const socket = io('http://localhost:4000');
+  const [username, setUsername] = useState<string>("");
+  const [userData, setUserData] = useState<string[] | string>([]);
+  const [chatuser, setChatUser] = useState<string>("");
+  const [chat, setChat] = useState<string[]>([]);
+  const [socket, setSocket] = useState<Socket | null>(null);
 
   useEffect(() => {
-
-    socket.on('connect', () => {
-      console.log('Socket connected:', socket.id);
+    const newSocket = io('http://localhost:4000');
+    setSocket(newSocket);
+    
+    newSocket.on('connect', () => {
+      console.log('Socket connected:', newSocket.id);
     });
 
-    socket.on('join',(data:string):void => {
+    newSocket.on('join',(data:string):void => {
       console.log('Received from server:', data);
     });
 
-    return () => {
+    newSocket.on('chatUsers', (userData:string[] | string):void => {
+      console.log('Chat Users:', userData);
+    });
+
+    return() => {
       console.log('Disconnecting socket...');
-      socket.disconnect();
+      newSocket.disconnect();
     };
   }, []);
   
   return (<div>
       <button onClick={()=>{
-        socket.emit('manuka', 'Hello from Client!');
+        if(socket) socket.emit('manuka', 'Hello from client!');
       }}>Send Message to Server</button>
+      <div>
+        <p>Chat Register</p>
+        <input placeholder='your username' onChange={(e: ChangeEvent<HTMLInputElement>)=>setUsername(e.target.value)}/>
+        <button onClick={()=>{
+          if(socket) socket.emit('usernameRegister', username);
+        }}>Register</button>
+      </div>
+      <div>
+        <p>chat</p>
+        <select onChangeCapture={(e: ChangeEvent<HTMLSelectElement>)=>setChatUser(e.target.value)}>
+          {userData?.map((user:string):JSX.Element=><option value={user}></option>)}
+        </select>
+
+      </div>
     </div>
   );
 }
